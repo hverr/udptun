8046ee96749ec66b9dc93fa337fb378e3a7a42bf
Henri Verroken <henriverroken@gmail.com>
2015-12-24 14:50:05 +0100

    Move tuntap code to Tundev module

---
+++ bin/tundev.ml
@@ -0,0 +1,21 @@
+open Core.Std
+open Async.Std
+
+type t = {
+  name : string;
+  reader : Reader.t;
+  writer : Writer.t;
+}
+
+let create () =
+  let file, name = Tuntap.opentun () in
+  let fd = Fd.create Fd.Kind.Char file (Info.of_string name) in
+  let reader = Reader.create fd in
+  let writer = Writer.create fd in
+  {name; reader; writer}
+
+let reader t = t.reader
+let writer t = t.writer
+let name t = t.name
+
+let close t = Tuntap.closetun t.name
