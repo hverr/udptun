89979a12efbaacc0202a523003fa7b331c5a8239
Henri Verroken <henriverroken@gmail.com>
2015-12-24 13:19:22 +0100

    Send UDP packets using async

---
+++ bin/main.ml
@@ -0,0 +1,37 @@
+open Core.Std
+open Cmdliner
+
+let default_port = 7777
+
+let local_address =
+  let doc = "The address to listen on." in
+  Arg.(value & opt string "0.0.0.0" & info ["a"; "local-address"] ~doc)
+
+let local_port =
+  let doc = "The port to listen on." in
+  Arg.(value & opt int default_port & info ["p"; "local-port"] ~doc)
+
+let remote_address =
+  let doc = "The remote address to connect to." in
+  let flags = ["A"; "remote-address"] in
+  Arg.(required & opt (some string) None & info flags ~doc)
+
+let remote_port =
+  let doc = "The remote port to connect to." in
+  let flags = ["P"; "remote-port"] in
+  Arg.(value & opt int default_port & info flags ~doc)
+
+let term = Term.(const Udptun.main $
+                 local_address $
+                 local_port $
+                 remote_address $
+                 remote_port)
+
+let info =
+  let doc = "create a udp tunnel on a newly created tun-device" in
+  Term.info "udptun" ~doc
+
+let () =
+  match Term.eval (term, info) with
+  | `Error _ -> exit 1
+  | _ -> exit 0
---
+++ bin/udptun.ml
@@ -0,0 +1,66 @@
+open Core.Std
+open Async.Std
+
+module Tun = struct
+  type t = {
+    name : string;
+    reader : Reader.t;
+    writer : Writer.t;
+  }
+
+  let create () =
+    let file, name = Tuntap.opentun () in
+    let fd = Fd.create Fd.Kind.Char file (Info.of_string name) in
+    let reader = Reader.create fd in
+    let writer = Writer.create fd in
+    {name; reader; writer}
+
+  let reader t = t.reader
+  let writer t = t.writer
+  let name t = t.name
+
+  let close t = Tuntap.closetun t.name
+end
+
+module UdpAsync = struct
+  module Client = struct
+    type t = {
+      fd : Fd.t;
+      address : Unix.Socket.Address.Inet.t;
+    }
+
+    let connect address =
+      let socket = Unix.Socket.(create Type.udp) in
+      Unix.Socket.connect socket address
+      >>| (fun socket ->
+        let fd = Unix.Socket.fd socket in
+        { fd; address }
+      )
+
+    let sendto t data =
+      let buf = Iobuf.of_string data in
+      let sender = Or_error.ok_exn (Udp.sendto ()) in
+      sender t.fd buf t.address
+  end
+end
+
+let to_address ip port =
+  let a = Unix.Inet_addr.of_string ip in
+  Unix.Socket.Address.Inet.create a ~port
+
+let main local_address local_port remote_address remote_port =
+  let start_sending () =
+    let address = to_address remote_address remote_port in
+    UdpAsync.Client.connect address >>= (fun client ->
+      let rec keep_sending () =
+        printf "Sending message\n%!";
+        UdpAsync.Client.sendto client "Hello World!" >>= fun () ->
+        after (Time.Span.of_sec 1.0) >>= fun () ->
+        keep_sending ()
+      in
+      keep_sending ();
+    )
+  in
+  ignore (start_sending ());
+  
+  never_returns (Scheduler.go ())
