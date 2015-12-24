4792bb56f9b497b3c63f215d652ab7b7f2924333
Henri Verroken <henriverroken@gmail.com>
2015-12-24 15:59:13 +0100

    Setup full redirection

--- bin/udptun.ml
+++ bin/udptun.ml
@@ -1,31 +1,38 @@
 open Core.Std
 open Async.Std
 
+let copy_reader_to_txer r txer =
+  let buf = String.create Tunnel.Txer.packet_size in
+  let rec copier () =
+    Reader.read r buf >>= function
+    | `Eof -> return ()
+    | `Ok l -> begin
+      let iobuf = Iobuf.(sub_shared ~len:l (of_string buf)) in
+      Tunnel.Txer.send_buf txer iobuf >>= fun () ->
+      copier ()
+    end
+  in
+  copier ()
+
+let copy_rxer_to_writer rxer w =
+  let f buf addr = Writer.write w (Iobuf.to_string buf) in
+  Tunnel.Rxer.start rxer f
+
 let to_address ip port =
   let a = Unix.Inet_addr.of_string ip in
   Unix.Socket.Address.Inet.create a ~port
 
 let main local_address local_port remote_address remote_port =
+  let tundev = Tundev.create () in
   let start_receiving () =
-    let handle_packet buf addr =
-      printf "Got packet from %s\n%!"
-        (Unix.Socket.Address.Inet.to_string addr)
-    in
     let address = to_address local_address local_port in
     Tunnel.Rxer.create address >>= fun rxer ->
-    Tunnel.Rxer.start rxer handle_packet
+    copy_rxer_to_writer rxer (Tundev.writer tundev)
   in
   let start_sending () =
     let address = to_address remote_address remote_port in
-    Tunnel.Txer.connect address >>= (fun txer ->
-      let rec keep_sending () =
-        printf "Sending message\n%!";
-        Tunnel.Txer.send_string txer "Hello World!" >>= fun () ->
-        after (Time.Span.of_sec 1.0) >>= fun () ->
-        keep_sending ()
-      in
-      keep_sending ();
-    )
+    Tunnel.Txer.connect address >>= fun txer ->
+    copy_reader_to_txer (Tundev.reader tundev) txer
   in
   ignore (start_receiving ());
   ignore (start_sending ());
