6f6ea9131561913cf746f957e253594de7a8b666
Henri Verroken <henriverroken@gmail.com>
2015-12-26 01:28:16 +0100

    Setup ppx_bitstring

--- bin/tundev.ml
+++ bin/tundev.ml
@@ -1,6 +1,16 @@
 open Core.Std
 open Async.Std
 
+module Packet = struct
+  type t = {
+    destination : Unix.Inet_addr.t;
+    raw : string;
+  }
+
+  let destination t = t.destination
+  let raw t = t.raw
+end
+
 type t = {
   name : string;
   reader : Reader.t;
@@ -19,3 +29,10 @@ let writer t = t.writer
 let name t = t.name
 
 let close t = Tuntap.closetun t.name
+
+let read_packet t =
+  let str = String.create 20 in
+  let bs = Bitstring.bitstring_of_string str in
+  match%bitstring bs with
+  | {| v : 4 |} -> v
+  | {| _ |} -> raise (Failure "Unknown format")
