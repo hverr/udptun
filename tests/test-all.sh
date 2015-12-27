b0416322ebb80f28e01199404cd614c9d6780ed9
Henri Verroken <henriverroken@gmail.com>
2015-12-27 15:14:28 +0100

    Add script to run all integration tests

---
+++ tests/test-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run {
+    echo "========= RUNNING INTEGRATION TEST $1 ========="
+    echo ""
+    if ! "$1"; then
+        "$1" clean
+        echo "FAILURE FOR TEST $1" >&2
+        exit 1
+    fi
+    echo ""
+}
+
+run "./test-one-to-one.sh"
+run "./test-one-to-many.sh"
+run "./test-one-to-one-hosts-url.sh"
+run "./test-one-to-many-hosts-url.sh"
