04b954750d7e7aef556e2acaf324293bd487933c
Henri Verroken <henriverroken@gmail.com>
2016-01-09 18:52:29 +0100

    Fix invalid ip packets test

--- tests/test-invalid-ip-packets.sh
+++ tests/test-invalid-ip-packets.sh
@@ -19,14 +19,14 @@ set -ex
 make
 
 # Create dockers
-#docker run -itd --name udptun1 --cap-add=NET_ADMIN --device=/dev/net/tun udptun /bin/bash
-#docker run -itd --name udptun2 --cap-add=NET_ADMIN --device=/dev/net/tun udptun /bin/bash
+docker run -itd --name udptun1 --cap-add=NET_ADMIN --device=/dev/net/tun udptun /bin/bash
+docker run -itd --name udptun2 --cap-add=NET_ADMIN --device=/dev/net/tun udptun /bin/bash
 IP1=$(docker inspect udptun1 | jq -r ".[0].NetworkSettings.IPAddress")
 IP2=$(docker inspect udptun2 | jq -r ".[0].NetworkSettings.IPAddress")
 
 # Start udptun
-#docker exec -d udptun1 /usr/local/bin/udptun -A "$IP2" -d udptun
-#docker exec -d udptun2 /usr/local/bin/udptun -A "$IP1" -d udptun
+docker exec -d udptun1 /usr/local/bin/udptun -A "$IP2" -d udptun
+docker exec -d udptun2 /usr/local/bin/udptun -A "$IP1" -d udptun
 
 # Give applications some time to create network devices
 sleep 1
@@ -59,7 +59,6 @@ echo $IPV4_PACKET | docker exec -i udptun2 xxd -r -p | hexdump -C \
 # Kernel can't handle it!
 docker exec udptun2 ping -W 3 -c 3 192.168.111.1
 
-exit 0
 # Clean up
 if [ "$1" != "--no-clean" ]; then
     "$0" clean
