dc34c08730f3b60e39689a4aa0f89e4c5c84cb9b
Henri Verroken <henriverroken@gmail.com>
2016-01-17 02:01:20 +0100

    Allow for non dockerized and i386 builds

--- deb/gen.sh
+++ deb/gen.sh
@@ -11,14 +11,34 @@ fi
 UNAME_M=$(uname -m)
 if [ $UNAME_M = x86_64 ]; then
     ARCH=amd64
+elif [ $UNAME_M = i686 ]; then
+    ARCH=i386
 else
     echo "Unsupported architecture: $UNAME_M" >&2
     exit 1
 fi
 
-cd
+ARGS=$(getopt -o "" --long local -- "$@")
+eval set -- "$ARGS"
+while true; do
+    case "$1" in
+        --local) LOCAL=1; shift ;;
+	--) shift; break ;;
+        *) echo "getopt: error: unsupported flag $1" >&2; exit 1;;
+    esac
+done
+
+if [ "$LOCAL" != 1 ]; then
+    cd
+fi
 
 mkdir -p udptun/DEBIAN
+
+if [ "$LOCAL" = 1 ]; then
+    pushd .. && make && popd
+    mkdir -p udptun/usr/bin/
+    cp ../udptun.native ./udptun/usr/bin/udptun
+fi
 
 cat > udptun/DEBIAN/control <<EOF
 Package: udptun
@@ -34,7 +54,9 @@ Section: net
 Homepage: https://github.com/hverr/udptun
 EOF
 
-cd
+if [ "$LOCAL" != 1 ]; then
+    cd
+fi
 dpkg-deb --build udptun >&2
 
 dpkg -i --force-depends udptun.deb >&2
@@ -42,4 +64,9 @@ apt-get install -f -y >&2
 
 /usr/bin/udptun --help &>/dev/null
 
-cat udptun.deb
+if [ "$LOCAL" != 1 ]; then
+    cat udptun.deb
+else
+    mv udptun.deb udptun_${VERSION}_$(uname -m).deb
+    rm -rf udptun
+fi
